2d9d5f15aa6f1cb04dfd91b07f804fd2067a36c9
Pʀᴀᴛʜᴍᴇsʜ <prathmeshcoder69@gmail.com>
2026-05-08 03:52:23 +0530

    Update search.js

--- src/scrapers/search.js
+++ src/scrapers/search.js
@@ -9,7 +9,7 @@ const searchScraper = async (keyword, page = 1) => {
     const anime = []
     $("#aa-movies li.series").each((_, el) => {
         const title = $(el).find("h2.entry-title").text().trim()
-        const anime_id = $(el).find("a.lnk-blk").attr("href").replace("https://toonstream.dad/series/", "").replace("/", "")
+        const anime_id = $(el).find("a.lnk-blk").attr("href").replace("${url}/series/", "").replace("/", "")
         const imgTag = $(el).find("img");
 
             let poster =
